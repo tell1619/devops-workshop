6987b95e93f5984b313c5407a0f47a3ca0557a69
tell1619 <tell1619@proton.me>
2024-01-24 16:54:27 +0000

    Amending region and resource names - V1-EC2.tf

--- terraform_code/V1-EC2.tf
+++ terraform_code/V1-EC2.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-1"
+  region = "eu-west-1"
 }
 
-resource "aws_instance" "demo-server" {
-    ami = "ami-022e1a32d3f742bd8"
+resource "aws_instance" "terraform-demo-server" {
+    ami = "ami-0fef2f5dd8d0917e8"
     instance_type = "t2.micro"
     key_name = "tds"
 }
